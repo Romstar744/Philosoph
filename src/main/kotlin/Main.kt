6be42c5b50386b15bb282a1c7ef8bc3296527b45
Romstar744 <romstar742@gmail.com>
2023-11-16 14:21:46 +0300

    23

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,70 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.concurrent.locks.ReentrantLock
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+var countPhilosopher = 0
+
+class Philosopher(val id: Int, val leftFork: ReentrantLock, val rightFork: ReentrantLock) : Runnable {
+    override fun run() {
+        eat()
+    }
+
+    private fun eat() {
+        var tut = true
+        while (tut) {
+            when ((1..2).random()) {
+                1 -> {
+                    if (rightFork.tryLock()) {
+                        println("Философ $id обедает левой вилкой.")
+                        tut = false
+                    } else if (!rightFork.tryLock() && !leftFork.tryLock()) {
+                        tut = false
+                        println("Философ $id размышляет.")
+                    }
+                }
+
+                2 -> {
+                    if (leftFork.tryLock()) {
+                        println("Философ $id обедает правой вилкой.")
+                        tut = false
+                    }else if (!rightFork.tryLock() && !leftFork.tryLock()) {
+                        tut = false
+                        println("Философ $id размышляет.")
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+fun main() {
+    println("Сколько философов за круглым столом: ")
+    println("Введите целое число: ")
+    countPhilosopher = enter().countPhilosopher
+
+    val forks = List(countPhilosopher) { ReentrantLock() }
+
+    val philosophers = List(countPhilosopher) { id ->
+        Philosopher(
+            id,
+            forks[id],
+            forks[(id + 1) % forks.size]
+        )
+    }
+    philosophers.forEach { Thread(it).start() }
+}
+//}
+
+class enter {
+    var countPhilosopher = 0
+    init {
+        var valid = false
+        do {
+            try {
+                countPhilosopher = readLine()?.toInt() ?: 0
+                valid = true
+            } catch (e: NumberFormatException) {
+                println("Ошибка. Введите целое число.")
+            }
+        } while (!valid)
+    }
 }
